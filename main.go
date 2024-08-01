b237d39b3458bcc374dfd1a7c78dc5e48f745e48
Artzy1401 <afifarsyi@gmail.com>
2024-08-01 12:04:55 +0700

    Create Base Project BackEnd Golang

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+// @title API CarZone E-Commerce
+// @version 1.0
+// @description This is a sample server for carzone e-commerce API.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /api
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
