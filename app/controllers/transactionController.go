5b9fd7380f06c233a6d474c9ad872150d9bef9c1
RayhanPJ <rayhanputra100@gmail.com>
2024-08-05 11:49:36 +0700

    create model and controller for order and transaction

---
+++ app/controllers/transactionController.go
@@ -0,0 +1,166 @@
+package controllers
+
+import (
+	"be-car-zone/app/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type TransactionController struct {
+	DB *gorm.DB
+}
+
+// FindAll godoc
+// @Summary Get all transactions
+// @Description Get all transactions
+// @Tags transactions
+// @Produce json
+// @Param Authorization header string true "Authorization. How to input in swagger : 'Bearer <insert_your_token_here>'"
+// @Success 200 {object} models.Transaction
+// @Router /api/cms/transactions [get]
+func (ctrl *TransactionController) FindAll(c *gin.Context) {
+	var transactions []models.Transaction
+	if err := ctrl.DB.Preload("Order.Car").Find(&transactions).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	var transactionDetails []models.TransactionDetail
+	for _, transaction := range transactions {
+		transactionDetails = append(transactionDetails, models.TransactionDetail{
+			ID:              transaction.ID,
+			OrderID:         transaction.OrderID,
+			PaymentProvider: transaction.PaymentProvider,
+			NoRek:           transaction.NoRek,
+			Amount:          transaction.Amount,
+			TransactionDate: transaction.TransactionDate,
+			CreatedAt:       transaction.CreatedAt,
+			UpdatedAt:       transaction.UpdatedAt,
+			Order: models.OrderDetail{
+				ID:         transaction.Order.ID,
+				UserID:     transaction.Order.UserID,
+				CarID:      transaction.Order.CarID,
+				TotalPrice: transaction.Order.TotalPrice,
+				Status:     transaction.Order.Status,
+				CreatedAt:  transaction.Order.CreatedAt,
+				UpdatedAt:  transaction.Order.UpdatedAt,
+				Car: models.CarDetail{
+					ID:          transaction.Order.Car.ID,
+					Name:        transaction.Order.Car.Name,
+					Description: transaction.Order.Car.Description,
+					Price:       transaction.Order.Car.Price,
+					TypeID:      transaction.Order.Car.TypeID,
+					BrandID:     transaction.Order.Car.BrandID,
+					IsSecond:    transaction.Order.Car.IsSecond,
+					CreatedAt:   transaction.Order.Car.CreatedAt,
+					UpdatedAt:   transaction.Order.Car.UpdatedAt,
+				},
+			},
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": transactionDetails})
+}
+
+// FindByID godoc
+// @Summary Get transaction by id
+// @Description Get transaction by id
+// @Tags transactions
+// @Produce json
+// @Param Authorization header string true "Authorization. How to input in swagger : 'Bearer <insert_your_token_here>'"
+// @Param id path string true "Transaction ID"
+// @Success 200 {object} models.Transaction
+// @Router /api/cms/transactions/{id} [get]
+func (ctrl *TransactionController) FindByID(c *gin.Context) {
+	var transaction models.Transaction
+	if err := ctrl.DB.Where("id = ?", c.Param("id")).First(&transaction).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "record not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": transaction})
+}
+
+// Create godoc
+// @Summary Create new transaction
+// @Description Create new transaction
+// @Tags transactions
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "Authorization. How to input in swagger : 'Bearer <insert_your_token_here>'"
+// @Param transaction body models.Transaction true "Transaction Data"
+// @Success 200 {object} models.Transaction
+// @Router /api/cms/transactions [post]
+func (ctrl *TransactionController) Create(c *gin.Context) {
+	var transaction models.Transaction
+
+	if err := c.ShouldBindJSON(&transaction); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := ctrl.DB.Create(&transaction).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": transaction})
+}
+
+// Update godoc
+// @Summary Update transaction
+// @Description Update transaction
+// @Tags transactions
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "Authorization. How to input in swagger : 'Bearer <insert_your_token_here>'"
+// @Param id path string true "Transaction ID"
+// @Param transaction body models.Transaction true "Transaction Data"
+// @Success 200 {object} models.Transaction
+// @Router /api/cms/transactions/{id} [put]
+func (ctrl *TransactionController) Update(c *gin.Context) {
+	var transaction models.Transaction
+	if err := ctrl.DB.Where("id = ?", c.Param("id")).First(&transaction).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "record not found"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&transaction); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := ctrl.DB.Save(&transaction).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": transaction})
+}
+
+// Delete godoc
+// @Summary Delete transaction
+// @Description Delete transaction
+// @Tags transactions
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "Authorization. How to input in swagger : 'Bearer <insert_your_token_here>'"
+// @Param id path string true "Transaction ID"
+// @Success 200 {object} models.Transaction
+// @Router /api/cms/transactions/{id} [delete]
+func (ctrl *TransactionController) Delete(c *gin.Context) {
+	var transaction models.Transaction
+	if err := ctrl.DB.Where("id = ?", c.Param("id")).First(&transaction).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "record not found"})
+		return
+	}
+
+	if err := ctrl.DB.Delete(&transaction).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "deleted successfully!"})
+}
--- app/models/orderModel.go
+++ app/models/orderModel.go
@@ -16,3 +16,26 @@ type Order struct {
 	User User `json:"-"`
 	Car  Car  `json:"-"`
 }
+
+type OrderDetail struct {
+	ID         uint      `json:"id"`
+	UserID     uint      `json:"user_id"`
+	CarID      uint      `json:"car_id"`
+	TotalPrice float64   `json:"total_price"`
+	Status     bool      `json:"status"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	Car        CarDetail `json:"car"`
+}
+
+type CarDetail struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Price       float64   `json:"price"`
+	TypeID      uint      `json:"type_id"`
+	BrandID     uint      `json:"brand_id"`
+	IsSecond    bool      `json:"is_second"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
