6fe0e3c4ae6db248b64202934fa0b6162a7317af
Rafi <rafifarhansabkiya@gmail.com>
2024-08-09 15:08:47 +0700

    fix main.go

--- main.go
+++ main.go
@@ -2,22 +2,21 @@ package main
 
 import (
 	"be-car-zone/api"
-	"be-car-zone/app/pkg/utils"
-	"log"
+	"net/http"
 )
 
-func main() {
-	// Menentukan port untuk server
-	port := utils.Getenv("HOST", "localhost:8080")
-
-	// Menjalankan server HTTP
-	log.Printf("Server running on %s", port)
-	err := api.App.Run(port)
-	if err != nil {
-		log.Fatal("Error starting server: ", err)
-	}
-}
-
 // func main() {
-// 	http.HandleFunc("/", api.Handler)
+// 	// Menentukan port untuk server
+// 	port := utils.Getenv("HOST", "localhost:8080")
+
+// 	// Menjalankan server HTTP
+// 	log.Printf("Server running on %s", port)
+// 	err := api.App.Run(port)
+// 	if err != nil {
+// 		log.Fatal("Error starting server: ", err)
+// 	}
 // }
+
+func main() {
+	http.HandleFunc("/", api.Handler)
+}
